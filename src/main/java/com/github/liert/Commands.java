bb9b37b0b56c2f905b69d3afbf802dba3f1db735
liert <2102156704@qq.com>
2024-02-28 22:55:41 +0800

    1.1 开发中

--- src/main/java/com/github/liert/Commands.java
+++ src/main/java/com/github/liert/Commands.java
@@ -1,11 +1,14 @@
 package com.github.liert;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 public class Commands implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String arg, String[] args) {
         if (commandSender instanceof Player) {
@@ -15,15 +18,18 @@ public class Commands implements CommandExecutor {
         if (args.length < 1) {
             Manage.sendMessage(commandSender, "/develop port <number>");
             Manage.sendMessage(commandSender, "/develop stop");
+            Manage.sendMessage(commandSender, "/develop plugin list");
+            Manage.sendMessage(commandSender, "/develop unload pluginName");
+            Manage.sendMessage(commandSender, "/develop load pluginName");
             return true;
         }
         if (args[0].equals("port") && args.length == 2) {
             if (!(commandSender instanceof Player)) {
-                Manage.sendMessage(commandSender, Manage.format("Develop", "非玩家禁止此使用命令."));
+                Manage.sendMessage(commandSender, Manage.format("Develop", "非玩家禁止使用此命令."));
                 return true;
             }
             if (Develop.getInstance().getConfig().getBoolean("AutoListener")) {
-                Manage.sendMessage(commandSender, Manage.format("Develop", "AutoListener 模式禁止此使用命令."));
+                Manage.sendMessage(commandSender, Manage.format("Develop", "AutoListener 模式禁止使用此命令."));
                 return true;
             }
             Develop.p = (Player) commandSender;
@@ -34,6 +40,27 @@ public class Commands implements CommandExecutor {
             }
             Server server = new Server(port);
             server.connect();
+            return true;
+        }
+        if (args[0].equals("unload") && args.length == 2) {
+            if (!Bukkit.getPluginManager().isPluginEnabled(args[1])) {
+                Manage.sendMessage(commandSender, Manage.format("Develop", args[1] + " 未启用."));
+                return true;
+            }
+            Plugin plugin = Bukkit.getPluginManager().getPlugin(args[1]);
+            Manage.sendMessage(commandSender, Manage.unload(plugin));
+            return true;
+        }
+        if (args[0].equals("load") && args.length == 2) {
+            if (!Manage.isunLoadPlugin(args[1])) {
+                Manage.sendMessage(commandSender, Manage.format("Develop", args[1] + " 未找到."));
+                return true;
+            }
+            Manage.sendMessage(commandSender, Manage.format("Develop", Manage.load(args[1])));
+            return true;
+        }
+        if (args[0].equals("plugin") && args[1].equals("list")) {
+            Manage.sendMessage(commandSender, Manage.format("Develop", Manage.getPlugins()));
             return true;
         }
         if (args[0].equals("stop") && args.length == 1) {
